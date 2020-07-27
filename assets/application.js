81a056da1b1a165ea07db1d1a297d1625ca0986e
ousmane-amadou <ousmane.amadou89@gmail.com>
2020-07-26 21:17:29 -0400

    REsponsive side drawer functionality implemented

--- assets/application.js
+++ assets/application.js
@@ -1,10 +1,20 @@
 // Put your applicaiton javascript here
 function call_to_action() {
-    console.log("hi");
     var home_section =  document.getElementById('1');
     var landing_section =  document.getElementById('2');
     
     home_section.className = "home";
     landing_section.className = "inactive";
 } 
+
+
+function openDrawer() {
+    var side_nav =  document.getElementsByClassName("side-nav")[0];
+    side_nav.style.width = "100vw";
+}
+
+function closeDrawer() {
+    var side_nav =  document.getElementsByClassName("side-nav")[0];
+    side_nav.style.width = "0vw";
+}
 
