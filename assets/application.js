ac9be0b5a774708266ba9c6a0ce11e670c0e4789
ousmane-amadou <ousmane.amadou89@gmail.com>
2020-07-26 20:47:38 -0400

    Got landing page connected to index

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,10 @@
 // Put your applicaiton javascript here
+function call_to_action() {
+    console.log("hi");
+    var home_section =  document.getElementById('1');
+    var landing_section =  document.getElementById('2');
+    
+    home_section.className = "home";
+    landing_section.className = "inactive";
+} 
+
